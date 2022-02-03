6d1c2e757f816155c6e9bd25044bd88c46577245
ozgeergezer <ozgeergezerrr@gmail.com>
2022-02-03 10:25:20 +0300

    .

--- NBuyFirstGroup.Core/domain/DomainEvent.cs
+++ NBuyFirstGroup.Core/domain/DomainEvent.cs
@@ -10,6 +10,10 @@ namespace NbuyFirstGroup.Core.domain
     public static class DomainEvent
     {
         /*Bu sınıfı IOC ile singleton bir instancea bağlayacağız uygulama ilk ayağa kalktığında bu dispatcher sınıgı static tanımlamamızın sebebi tek bir instance ile uygulama genelinde bu sevk edici arkadaşı çalıştırmak. IOC üzerinden tanımlanmış tüm handlerlara dispatcher üzerindeki Dispatch methodu ile eerişip tetiklenmelkerini sağlayacağız.Fakat bunu bu katmanda değil uygulamanın infrastructure katmanında yapacağız.*/
+
+        //DomainEvent taskın bir özelliği değiştiği zaman kullanıcılara mail vs. bir şey gitmesi lazıkm
+        //bir state değişiminde entitydeki event fırlatman lazım.
+
         private static IDomainEventDispatcher<IDomainEvent> domainEventDispatcher { get; set; }
 
         public static void Raise(IDomainEvent @event)
--- NBuyFirstGroup.Core/logging/ILogServices.cs
+++ NBuyFirstGroup.Core/logging/ILogServices.cs
@@ -13,7 +13,7 @@ namespace NBuyFirstGroup.Core.logging
         public const string Warning = "Warning";
         public const string Success = "Success";
     }
-
+    // log tablosunda login işlemlerini tutacağız daha sonrasında hangi operasyonda neler olmuş görebilmek için.
     // yapılan işlemlere ait kayıtları izleyebilmek için bu servisin log methodunu kullanacağız. message ve logLevel loglama kriktiklik seviyesi
     public interface ILogServices
     {
